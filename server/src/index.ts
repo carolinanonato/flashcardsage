b3e34626274fdac05e5dc2db1fffd0f93abdb2ff
Carolina Nonato <csnonato@gmail.com>
2023-04-08 19:13:32 -0400

    setting mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,23 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+import DeckModel from './models/Deck';
+
+const PORT = 5000;
+
 const app = express()
 
-app.get('/hello', (req: Request, res: Response) => {
+
+
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 });
 
-app.listen(5000);
+app.get('/hello', (req: Request, res: Response) => {
+    res.send('GG')
+});
+
+mongoose.connect('mongodb+srv://flashcard:CzCDImDWnjZhMlhn@mycluster.tdlwnkt.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log(`Listening on port ${PORT}`);
+    app.listen(PORT);
+})
