e28c7568dba88d5c6a7b6caafb4c984afed35f8b
Carolina Nonato <csnonato@gmail.com>
2023-04-08 21:04:11 -0400

    fixing env

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 
 import Deck from './models/Deck';
+import { config } from 'dotenv';
+config();
 
 const PORT = 5000;
 
@@ -18,7 +20,7 @@ app.post('/decks', async (req: Request, res: Response) => {
     res.json(createdDeck);
 });
 
-mongoose.connect('mongodb+srv://flashcard:CzCDImDWnjZhMlhn@mycluster.tdlwnkt.mongodb.net/?retryWrites=true&w=majority').then(() => {
+mongoose.connect(process.env.MONGO_URL!).then(() => {
     console.log(`Listening on port ${PORT}`);
     app.listen(PORT);
 })
