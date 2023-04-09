337669d845edadd762b3e7577aac1ec735517772
Carolina Nonato <csnonato@gmail.com>
2023-04-09 19:51:07 -0400

    ui: delete button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ const [decks, setDecks] = useState<TDeck[]>([])
 
 async function handleCreateDeck(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-   await fetch('http://localhost:5000/decks', {
+   const response = await fetch('http://localhost:5000/decks', {
     method: 'POST',
     body: JSON.stringify({ 
       title,
@@ -23,7 +23,16 @@ async function handleCreateDeck(e: React.FormEvent<HTMLFormElement>) {
       "Content-Type": "application/json"
     }
    })
+   const deck = await response.json()
+   setDecks([...decks, deck])
    setTitle('')
+}
+
+async function handleDeleteDeck(deckId: string) {
+  await fetch(`http://localhost:5000/decks/${deckId}`, {
+    method: 'DELETE',   
+   })
+   setDecks(decks.filter((deck) => deck._id !== deckId))
 }
 
 useEffect(() => {
@@ -40,7 +49,10 @@ useEffect(() => {
       <ul className="decks">
         {
         decks.map((deck) => (
-          <li key={deck._id}>{deck.title}</li>
+          <li key={deck._id}>
+            <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
+            {deck.title}
+            </li>
 
        ) )}
       </ul>
